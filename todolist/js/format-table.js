55ffccabb780d815aa58c9e0f34e0a55d65db4bf
Alexandr-Kravchenko <sanya5478@gmail.com>
2021-08-19 16:47:37 +0300

    fefactored code

--- todolist/js/format-table.js
+++ todolist/js/format-table.js
@@ -1,47 +1,47 @@
 function createTitle(title, status) {
-    let Title = document.createElement('td');
-    Title.className = "title";
-    Title.innerText = title;
-    if (status) Title.classList.add('done');
+    let titleNode = document.createElement('td');
+    titleNode.className = "title";
+    titleNode.innerText = title;
+    if (status) titleNode.classList.add('done');
 
-    return Title
+    return titleNode
 }
 
 function createDescription(description) {
-    let Description = document.createElement('td');
-    Description.className = "description";
-    Description.innerText = description;
+    let descriptionNode = document.createElement('td');
+    descriptionNode.className = "description";
+    descriptionNode.innerText = description;
 
-    return Description;
+    return descriptionNode;
 }
 
 function createStatus(status) {
-    let Status = document.createElement('td');
-    let Input = document.createElement('input');
-    Input.className = 'status';
-    Input.type = "checkbox";
-    Input.checked = status;
-    Status.append(Input);
+    let statusNode = document.createElement('td');
+    let inputNode = document.createElement('input');
+    inputNode.className = 'status';
+    inputNode.type = "checkbox";
+    inputNode.checked = status;
+    statusNode.append(inputNode);
 
-    return Status;
+    return statusNode;
 }
 
 function createDueDate(due_date) {
-    let Due_date = document.createElement('td');
-    Due_date.className = 'due-date';
+    let due_dateNode = document.createElement('td');
+    due_dateNode.className = 'due-date';
     if(due_date) {
-        if (isDue(due_date)) Due_date.classList.add('due');
-        Due_date.innerText = due_date.toLocaleDateString();
+        if (isDue(due_date)) due_dateNode.classList.add('due');
+        due_dateNode.innerText = due_date.toLocaleDateString();
     }
 
-    return Due_date;
+    return due_dateNode;
 }
 
-function createTerminator() {
-    let Terminator = document.createElement('td');
-    Terminator.className = 'terminator';
-    Terminator.innerHTML = '&#10006;';
-    return Terminator;
+function createDeleteButton() {
+    let terminatorNode = document.createElement('td');
+    terminatorNode.className = 'terminator';
+    terminatorNode.innerHTML = '&#10006;';
+    return terminatorNode;
 }
 
 function isDue(date) {
@@ -53,40 +53,30 @@ function resetTable(table) {
     table.innerHTML = '';
 }
 
+function createTodoRow(todo) {
+    const { id, title, description, status, due_date } = todo;
+    
+    let Todo = document.createElement('tr');
+    Todo.id = id;
+
+    Todo.append(
+        createTitle(title, status),
+        createDescription(description),
+        createStatus(status),
+        createDueDate(due_date),
+        createDeleteButton()
+    )
+
+    return Todo
+}
+
+
 function drawTodolist(todolist) {
     if(todolist.length > 0) {
-        let formattedArr = todolist.map((todo) => {
-            const { id, title, description, status, due_date } = todo;
-    
-            let Todo = document.createElement('tr');
-            Todo.id = id;
-    
-            let Title = createTitle(title, status);
-            Todo.append(Title);
-    
-            let Description = createDescription(description);
-            Todo.append(Description);
-    
-            let Status = createStatus(status);
-            Todo.append(Status);
-    
-            let Due_date = createDueDate(due_date);
-            Todo.append(Due_date);
-    
-            let Terminator = createTerminator();
-            Todo.append(Terminator);
-    
-            return Todo
-        })
-    
+        let rows = todolist.map(createTodoRow);
         let table = document.querySelector('.todolist tbody');
-        resetTable(table)
-    
-        for (let i = 0; i < formattedArr.length; i++) {
-            let tableBody = document.querySelector('.todolist tbody');
-            tableBody.append(formattedArr[i])
-        }
-        return formattedArr;
+        resetTable(table);
+        table.append(...rows);
     } else {
         return document.createElement('tr');
     }
